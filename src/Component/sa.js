63c88a9e3ac469eab61564a4b2a82973b546079b
kvenkat615 <knarasimha615@gmail.com>
2021-03-31 16:11:10 +0530

    fouth

---
+++ src/Component/sa.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class sa extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default sa
